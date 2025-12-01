534fae3854d944f258e1a83485cda61eb41cca8b
Mahasree-Chandrasekaran <mahasree093@gmail.com>
2025-12-01 16:05:11 +0530

    Select-statement

--- Select-statement-Practise.sql
+++ Select-statement-Practise.sql
@@ -1,4 +1,21 @@
 SELECT * FROM parks_and_recreation.employee_demographics;
-Select first_name, last_name from employee_demographics;
 
+Select first_name, last_name from employee_demographics;
 Select first_name, last_name, 'active' as status from employee_demographics;
+
+#Distinct
+select  distinct gender from employee_demographics;
+select distinct dept_id, salary from employee_salary;
+
+#Concatenation
+Select concat(first_name," ",last_name) as full_name from employee_demographics;
+Select concat(first_name,' ',last_name,' ','works here ') as full_name from employee_demographics;
+
+#Duplication
+Select first_name, first_name from employee_demographics;
+
+#Literal boolean
+Select first_name, (gender='Female') as Status from employee_demographics;
+
+
+
