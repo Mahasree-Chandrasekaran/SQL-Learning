57c323b5a1302ef1f7d8b9d3e461b02b0ede6bbe
Mahasree-Chandrasekaran <mahasree093@gmail.com>
2025-12-01 15:57:49 +0530

    Select-statement tutorial

---
+++ Select-statement-Practise.sql
@@ -0,0 +1,4 @@
+SELECT * FROM parks_and_recreation.employee_demographics;
+Select first_name, last_name from employee_demographics;
+
+Select first_name, last_name, 'active' as status from employee_demographics;
